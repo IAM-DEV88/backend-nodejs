c79631a1346a81fc8a9780d721e72b7298a6e01f
Andres Muñoz <141512518+IAM-DEV88@users.noreply.github.com>
2023-11-04 18:57:14 -0500

    server basico creado

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log('inicia backend-nodejs');
+const express = require('express')
+
+const app  = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Servidor ejecutado')
+})
+
+app.listen(port, () =>{
+  console.log('Puerto: ', port);
+})
+
